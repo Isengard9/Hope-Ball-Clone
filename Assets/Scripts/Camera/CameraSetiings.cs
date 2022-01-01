179719748d4409fb43ae44779d44ae8ba5f27392
Nurullah Çelik <66533548+ogrnurullahcelik@users.noreply.github.com>
2022-01-01 11:01:56 +0300

    Camera Settings
    Created ortographic camera size calculation function is created in Camera Settings script
    
    Player Manager script created

---
+++ Assets/Scripts/Camera/CameraSetiings.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This script using for Camera Settings
+/// 1-Calculating Ortographic camera size
+/// </summary>
+///
+[RequireComponent(typeof(Camera))]
+public class CameraSetiings : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = this.GetComponent<Camera>();
+        InitializeSettings();
+    }
+
+
+    /// <summary>
+    /// Initialize functions called
+    /// </summary>
+    void InitializeSettings()
+    {
+        CalculateOSize();
+    }
+    /// <summary>
+    /// Calculate Ortographic Camera size and set that
+    /// </summary>
+    void CalculateOSize()
+    {
+        float camOSize = 0;
+        camOSize = ((720.0f / 1280.0f) * 5) / ((float)Screen.width / (float)Screen.height);
+        mainCamera.orthographicSize = camOSize;
+
+    }
+
+}
